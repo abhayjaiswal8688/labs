c445818535066ee73d0348506a12960858978f3d
abhayjaiswal8688 <abhayjaiswal8688@gmail.com>
2025-06-09 12:04:22 +0530

    intializing the project

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,10 @@
+export function Hero(){
+    return <div className="grid grid-cols-12">
+        <div className="col-span-1">
+                <img src="/src/images/logo.jpg"/>
+        </div>
+        <div className="col-span-12 text-4xl">
+                SHRI MATA VAISHNO DEVI UNIVERSITY
+        </div>
+    </div>
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+## KUMAR'S LAB
+This application is a created for a client who want to showcase his team ,research work going on his lab and etc
+through this website he can
+- Add all details related to his lab like lab members , lab alumni ,resources etc
+- He can add project work which will be rendered on website
