e81c72cf7e5a88d95b3da238fe495e9313dad2eb
abhayjaiswal8688 <145705056+abhayjaiswal8688@users.noreply.github.com>
2025-08-05 19:53:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Hero } from './components/Hero'
-import { Navbar } from './components/navbar'
+import { Navbar } from './components/Navbar'
 import { Homepage } from './components/Homepage'
 import { Resources } from './components/Resources'
 import { Project } from './components/project'
